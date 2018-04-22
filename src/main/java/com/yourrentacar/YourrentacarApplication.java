1b5875d9c760244a219d04b04b54a1881646475e
Matheus Glauser <mglauser@yourtravel.com.br>
2018-04-22 14:08:39 -0300

    agora vai

---
+++ src/main/java/com/yourrentacar/YourrentacarApplication.java
@@ -0,0 +1,12 @@
+package com.yourrentacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourrentacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourrentacarApplication.class, args);
+	}
+}
